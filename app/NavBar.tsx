8b38a3a1cfbdc04528040163b575fc804dec6abb
Abubakar Siddique <abz.siddique@gmail.com>
2023-09-15 17:19:53 +0100

    refactor: accessing sessions on the client

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,17 +1,22 @@
+'use client'
 import React from 'react'
 import Link from 'next/link'
+import { useSession } from 'next-auth/react'
 
 const NavBar = () => {
+  const { status, data: session } = useSession();
+
   return (
     <div className="navbar bg-base-100 border rounded-lg">
       <div className="navbar-start">
         <Link href={'/'} className="btn btn-ghost normal-case text-xl">Next-App</Link>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
+        <ul className="menu menu-horizontal px-1 flex">
           <Link href={'/admin'} className="btn btn-ghost normal-case text-sm">Admin</Link>
           <Link href={'/users'} className="btn btn-ghost normal-case text-sm">Users</Link>
-          <Link href={'/api/auth/signin'} className="btn btn-ghost normal-case text-sm">Login</Link>
+            {status === 'unauthenticated' && <Link href={'/api/auth/signin'} className="btn btn-ghost normal-case text-sm">Login</Link>}
+            {status === 'authenticated' && <div className='px-4 self-center'>{session.user?.name}</div>}
         </ul>
       </div>
     </div>
